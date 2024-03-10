46f96b32a00202fc267e76db5dbd35e12bc3f2db
20220005 <20220005@itla.edu.do>
2024-03-10 10:09:29 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.tarea6',
+  appName: 'tarea6',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
