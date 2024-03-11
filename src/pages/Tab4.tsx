0631b641671c8ef293e7361e2eb6e99eedefb42c
20220005 <20220005@itla.edu.do>
2024-03-11 12:53:13 -0400

    arreglando estilos country

--- src/pages/Tab4.tsx
+++ src/pages/Tab4.tsx
@@ -8,9 +8,9 @@ import {
   IonItem,
   IonPage,
   IonRow,
+  IonSpinner,
   IonTitle,
-  IonToolbar,
-  IonSpinner 
+  IonToolbar
 } from "@ionic/react";
 import { useState } from "react";
 import "./Tab4.css";
@@ -18,18 +18,17 @@ import "./Tab4.css";
 const Tab4: React.FC = () => {
   const [country, setCountry] = useState("");
   const [university, setUniversity] = useState([]);
-  const [loading, setLoading] = useState(false); 
+  const [loading, setLoading] = useState(false);
 
-  function getCountry(country: string) {
-    if (!country.trim()) { 
-      return; 
+  function getCountry() {
+    if (!country.trim()) {
+      return;
     }
 
-    setLoading(true); 
+    setLoading(true);
     fetch(`http://universities.hipolabs.com/search?country=${country}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setUniversity(data);
         setLoading(false);
       });
@@ -43,51 +42,53 @@ const Tab4: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-  <div className="container-form">
-    <IonItem>
-      <IonInput
-        label="Country"
-        placeholder="Enter the country"
-        onIonChange={(e) => {
-          setCountry(e.detail.value!);
-        }}
-      ></IonInput>
-    </IonItem>
+        <div className="container-form">
+          <IonItem>
+            <IonInput
+              label="Country"
+              placeholder="Enter the country"
+              value={country}
+              onIonChange={(e) => {
+                setCountry(e.detail.value!);
+              }}
+            ></IonInput>
+          </IonItem>
+<div className="button-container">
+          <IonButton
+            onClick={() => {
+              getCountry();
+            }}
+          >
+            Get
+          </IonButton>
+          </div>
+        </div>
 
-    <IonButton
-      onClick={() => {
-        getCountry(country);
-      }}
-    >
-      Get
-    </IonButton>
-  </div>
+        <div className="table-container">
+          {loading ? (
+            <IonSpinner />
+          ) : (
+            <div className="card-container">
+          
+                  {university.map((uni: any, index) => (
 
-  <div className="table-container"> {/* Nuevo div contenedor de la tabla */}
-    {loading ? ( 
-      <IonSpinner />
-    ) : (
-      <IonGrid>
-        <IonRow>
-          <IonCol size="6">Name</IonCol>
-          <IonCol size="3">Domain</IonCol>
-          <IonCol size="3">Web</IonCol>
-        </IonRow>
-        {university.map((uni: any, index) => {
-          return (
-            <IonRow key={index}>
-              <IonCol size="6">{uni.name}</IonCol>
-              <IonCol size="3">{uni.domains}</IonCol>
-              <IonCol size="3">
-                <a href={uni.web_pages} target="_blank">{uni.web_pages}</a>
-              </IonCol>
-            </IonRow>
-          );
-        })}
-      </IonGrid>
-    )}
-  </div>
-</IonContent>
+                    <div className="card" key={index}>
+                    <div className="uni-name">
+                        <p>{uni.name}</p>
+                    </div>
+                    <div className="uni-data">
+                    <p>{uni.domains}</p>
+                    <a href={uni.web_pages[0]} target="_blank">{uni.web_pages[0]}</a>
+                    </div>
+                    </div>
+
+                 
+                  ))}
+                
+            </div>
+          )}
+        </div>
+      </IonContent>
     </IonPage>
   );
 };
